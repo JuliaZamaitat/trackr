26405fcef91753ad8e1a26ca611007f53450da98
JuliaZamaitat <j.zamaitat@gmail.com>
2021-12-02 12:54:06 +0100

    fixed routing issue.

--- server/src/routes/fileRoutes.ts
+++ server/src/routes/fileRoutes.ts
@@ -1,14 +1,9 @@
-import { Request, Response } from "express";
 
 const fileController = require("../controller/fileController"),
   router = require("express").Router();
 
-  
-  router.get("/", (req: Request, res: Response) => {
-    res.send("Juhu")
-  });
 
-// router.get("/:id", fileController.find);
+router.get("/:id", fileController.find);
 //routen für alle weiteren methoden hinzufügen
 
 module.exports = router;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,17 +3,9 @@ import { Request, Response } from "express";
 const router = require("express").Router(),
 fileRoutes = require("./fileRoutes");
 
-router.use("/files", function (req: Request, res: Response) {
-	res.send("GET request to the server");
-});
-// router.use("/files", fileRoutes);
+router.use("/files", fileRoutes);
 
-// router.use("/", fileRoutes);
 
 //fileversions benutzen
-
-
-
-
 
 module.exports = router;
