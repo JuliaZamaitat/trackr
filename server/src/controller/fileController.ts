dcbb0a1aa8788f2fe8b102e267a1870d97ab6f9d
parafoxx <38963705+parafoxx@users.noreply.github.com>
2022-01-08 11:58:02 +0000

    Backend tests (#40)
    add CI for backend

--- server/src/controller/fileController.ts
+++ server/src/controller/fileController.ts
@@ -4,7 +4,7 @@ const File = require("../model/file"),
   FileVersions = require("../model/fileVersions");
 
 module.exports = {
-  find: (req: Request, res: Response) => {  
+  find: (req: Request, res: Response) => {
     let fileId = req.params.fileId;
     File.findById(fileId)
       .then((file: any) => {
@@ -19,44 +19,44 @@ module.exports = {
   findAll: (req: Request, res: Response) => {
     let fileVersionsId = req.params.fileVersionsId;
     FileVersions.findById(fileVersionsId)
-    .then((fileVersions: any) => {
+      .then((fileVersions: any) => {
         const allFiles = fileVersions.files;
-        if (!allFiles){
-            res.status(404).send("No files found")
+        if (!allFiles) {
+          res.status(404).send("No files found")
         }
         res.status(200).json(allFiles);
-    })
-    .catch((error: Error) => {
+      })
+      .catch((error: Error) => {
         res.status(500).send(`Error fetching files: ${error.message}`);
         return;
-    });
+      });
   },
 
   create: (req: Request, res: Response) => {
     let fileVersionsId = req.params.fileVersionsId;
     let fileParams = {
-        title: req.body.title,
-        createdAt: Date.now(),
-        content: req.body.content
+      title: req.body.title,
+      createdAt: Date.now(),
+      content: req.body.content
     }
     File.create(fileParams)
-    .then((file: any) => {
+      .then((file: any) => {
         FileVersions.findByIdAndUpdate(fileVersionsId,
-            { $push: { files: file } },
-            { new: true }
+          { $push: { files: file } },
+          { new: true }
         ).populate("files")
-        .then((file: any) => {
+          .then((file: any) => {
             res.status(201).json(file)
-        })
-        .catch((error: Error) => {
+          })
+          .catch((error: Error) => {
             res.status(500).send(`Could not find parent file: ${error.message}`);
             return;
-        })
-    })
-    .catch((error: Error) => {
+          })
+      })
+      .catch((error: Error) => {
         res.status(500).send(`Error creating file version: ${error.message}`);
         return;
-    })
+      })
   },
 
   delete: (req: Request, res: Response) => {
@@ -66,14 +66,14 @@ module.exports = {
       { $pull: { files: { _id: fileId } } }
     ).then(() => {
       File.findByIdAndDelete(fileId)
-    .then((file: any) => {
-        res.status(200).send(`File deleted: ${fileId}`)
+        .then((file: any) => {
+          res.status(200).send(`File deleted: ${fileId}`)
+        })
     })
-    })
-    .catch((error: Error) => {
-      res.status(500).send(`Error deleting file by ID: ${error.message}`);
-      return;
-    })
+      .catch((error: Error) => {
+        res.status(500).send(`Error deleting file by ID: ${error.message}`);
+        return;
+      })
   },
 
   update: (req: Request, res: Response) => {
@@ -87,12 +87,12 @@ module.exports = {
       },
       { new: true }
     )
-    .then((file: File) => {
-      res.json(file)
-    })
-    .catch((error: Error) => {
-      res.status(500).send(`Could not update file: ${error.message}`);
-      return;
-    })
+      .then((file: File) => {
+        res.json(file)
+      })
+      .catch((error: Error) => {
+        res.status(500).send(`Could not update file: ${error.message}`);
+        return;
+      })
   }
 };
---
+++ server/src/tests/controller/fileVersionsController.spec.ts
@@ -0,0 +1,98 @@
+import mongoose from 'mongoose';
+import { Request, Response } from 'express';
+const FileVersions = require( "../../model/fileVersions");
+const File = require( "../../model/file");
+const fileVersionsController = require("../../controller/fileVersionsController")
+
+const mongodbURI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/trackr";
+
+describe('Get all files request', () => {
+  let mockRequest: Partial<Request>;
+  let mockResponse: any; //Partial<Response>;
+  let responseObject: any;
+  let mongoClient: typeof mongoose;
+
+  beforeAll(async () => {
+    mongoClient = await mongoose.connect(mongodbURI as string);
+  });
+
+  afterAll(async () => {
+    await mongoClient.connection.close();
+  });
+
+  beforeEach(() => {
+    mockRequest = {};
+    const mockResponseGenerator = () => {
+      const res: any = {};
+      res.status = jest.fn().mockImplementation((st) => {res.statusCode = st; return res;});
+      res.json = jest.fn().mockImplementation((json:string) => {res.responseSent(json) ; return res;});
+      res.send = jest.fn().mockImplementation((value: any) =>{
+        res.responseSent(value);
+      });
+      res.waitForResponse = new Promise((success,error) =>{
+        // @ts-ignore
+        res.responseSent = success;
+      });
+      return res;
+    };
+
+
+    mockResponse = mockResponseGenerator();
+  });
+
+  afterEach(async () => {
+  //  await mongoClient.connection.db.dropDatabase();
+    await FileVersions.deleteMany();
+    await File.deleteMany();
+  });
+
+  test('200 - files', (done) => {
+    const expectedStatusCode = 200;
+
+    try {
+      mockResponse.waitForResponse
+        .then((dataSent: any)  => {
+          expect(mockResponse.statusCode).toBe(expectedStatusCode);
+          done()
+        })
+    } catch (error) {
+      done(error);
+    }
+
+    fileVersionsController.findAll(mockRequest as Request, mockResponse as Response);
+  })
+
+
+  // @ts-ignore
+  test('response - files',   (done) => {
+    // this response cannot be returned
+
+    const expectedResponse = {
+      files: [
+        {
+          title: "foo bar"
+        },
+        {
+          title: "bar baz"
+        }
+      ]
+    };
+
+    try {
+      mockResponse.waitForResponse
+        .then((dataSent: any)  => {
+           expect(dataSent.length).toEqual(2);
+          done()
+        })
+    } catch (error) {
+      done(error);
+    }
+    const result =  FileVersions.create(expectedResponse.files)
+      .then((createResult:any) =>{
+    fileVersionsController.findAll(mockRequest as Request, mockResponse as Response);
+    })
+  })
+
+
+})
